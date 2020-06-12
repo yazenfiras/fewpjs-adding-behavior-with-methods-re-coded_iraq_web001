256c2751e0792ee8d2b2342119452e6815982ee8
yazen firas <psyazen3@gmail.com>
2020-06-12 19:03:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-// Your code here
-class Cat  {
-  constructor(name,sex ) {
-    this.name=name;
-    this.sex=sex;
 
+  
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex =  sex;
   }
-  speak(){
+
+  speak() {
     return `${this.name} says meow!`
   }
 }
 
-class Dog  {
-  constructor(name,sex) {
-    this.name=name;
-    this.sex=sex;
-
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex =  sex;
   }
-  speak(){
+
+  speak() {
     return `${this.name} says woof!`
   }
 }
 
-class Bird  {
-  constructor(name,sex ) {
-    this.name=name;
-    this.sex=sex;
-
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex =  sex;
   }
-  speak(gender){
-    if(this.sex=="male"){
 
-      return `It's me! ${this.name}, the parrot! `
-    }else{
-
-      return `${this.name}says squawk!`
+  speak() {
+    if (this.sex == "male") {
+      return `It's me! ${this.name}, the parrot!`
+    } else {
+      return `${this.name} says squawk!`
     }
-
-
   }
 }
+
